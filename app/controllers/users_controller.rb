496c4425269ddc6c31773e1eb739b4adf4dc59b5
Taryn Keim <tarynkeim@gmail.com>
2015-05-18 13:07:40 -0400

    updated devise params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,35 @@ class UsersController < ApplicationController
   end
 
   def profile
-    @user = User.find_by(name: params[:name].capitalize)
-   end
+    @user = User.find_by(username: params[:username].downcase)
+    if @user.nil?
+      @users = User.all
+      flash.now[:alert] = "Your Booksie was not found"
+      render 'landing'
+    end
+  end
 
-# protected
-#    def after_sign_in_path_for(resource)
-#      user_path(current_user) profile
-#    end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+        flash[:success] = "Welcome to Booksie!"
+        render 'profile'
+      else
+        render 'new'
+      end
+  end
+
+
+
+private
+ def user_params
+  params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+  end
+
 end
+
