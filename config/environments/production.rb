566c24304e806a4d04a2f2703c5a8be808e4ef11
Taryn Keim <tarynkeim@gmail.com>
2015-05-19 15:50:25 -0400

    updated the environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'https://fierce-thicket-5831.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'fierce-thicket-5831.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
