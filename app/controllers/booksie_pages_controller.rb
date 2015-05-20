082650ee66ed04db86b82e7302a2e20ac114ad37
Marilu Kernan <marilukernan@mac.com>
2015-05-20 16:22:24 -0400

    moving profile page to booksie page show

--- app/controllers/booksie_pages_controller.rb
+++ app/controllers/booksie_pages_controller.rb
@@ -1,2 +1,22 @@
 class BooksiePagesController < ApplicationController
+	before_action :set_booksie_page, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@booksie_page = current_user.booksie_pages.all
+	end
+
+	def show
+	end
+
+	def new
+		@booksie_page = current_user.booksie_pages.new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+
 end
