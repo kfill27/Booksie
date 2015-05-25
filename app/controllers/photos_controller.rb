7d3f5238885587e83be9a59dc539eba1c385d881
Kyra Fillmore <kfillmore27@gmail.com>
2015-05-25 14:55:28 -0400

    stylesheet changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -34,7 +34,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to :back, notice: 'Photo was successfully uploaded.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new }
