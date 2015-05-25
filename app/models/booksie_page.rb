e43f33c9284fd9d864481d3e9fb2ec1ed93c055d
Taryn Keim <tarynkeim@gmail.com>
2015-05-25 13:07:15 -0400

    fixed merge conflit

--- app/models/booksie_page.rb
+++ app/models/booksie_page.rb
@@ -1,10 +1,7 @@
 class BooksiePage < ActiveRecord::Base
   has_many :abilities, dependent: :destroy
   has_many :users, through: :abilities
-<<<<<<< HEAD
   has_many :photos, dependent: :destroy
-=======
-  has_many :photos
   has_many :videos
->>>>>>> video_uploads
+
 end
