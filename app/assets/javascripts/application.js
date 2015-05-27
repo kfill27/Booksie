4bb8fb6088e5635ef14ae568e35ab81dbd642507
Kyra Fillmore <kfillmore27@gmail.com>
2015-05-27 17:13:41 -0400

    fix navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,6 +24,12 @@
 // });
 
 // })
-
-
+<script>
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+</script>
 
