f07b9cb0c3238a7399905157251e5073fa5a0086
Taryn Keim <tarynkeim@gmail.com>
2015-05-27 07:39:20 -0400

    added comments to videos and milestones

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :photos, concerns: :commentable
   resources :videos, concerns: :commentable
+  resources :milestones, concerns: :commentable
 
 
   devise_for :users
