3a15ded94b72e27ca542a15fa2344af960358209
Marilu Kernan <marilukernan@mac.com>
2015-05-29 16:56:25 -0400

    fix of circles and milestone limit

--- app/models/booksie_page.rb
+++ app/models/booksie_page.rb
@@ -9,7 +9,7 @@ class BooksiePage < ActiveRecord::Base
   scope :contributor, ->(user) { joins(:ability).where(abilities: {role: "contributor"}) }
 
   def recent_milestones
-  	milestones.limit(10).sort_by(&:date).reverse
+  	milestones.limit(20).sort_by(&:date).reverse
   end
 
   def owner
