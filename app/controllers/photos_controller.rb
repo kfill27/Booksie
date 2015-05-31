346b75bf5bf4a75fde62f75ac601e9d8f67806a6
Taryn Keim <tarynkeim@gmail.com>
2015-05-31 13:16:44 -0400

    finished adding the caption

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -99,6 +99,6 @@ class PhotosController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
       # added date
-      params.require(:photo).permit(:date, :image, :booksie_page_id, :tag_list)
+      params.require(:photo).permit(:date, :image, :booksie_page_id, :tag_list, :caption)
     end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -74,6 +74,6 @@ class VideosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def video_params
-      params.require(:video).permit(:date, :video, :booksie_page_id, :tag_list)
+      params.require(:video).permit(:date, :video, :booksie_page_id, :tag_list, :caption)
     end
 end
